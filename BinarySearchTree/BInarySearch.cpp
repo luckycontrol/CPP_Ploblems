eaa9b6f09fa7ab1efe4c8a3cc86e5e3ba085dc48
luckycontrol <beansu1@naver.com>
2021-03-04 13:39:47 +0900

    cpp-practice

--- BinarySearchTree/BInarySearch.cpp
+++ BinarySearchTree/BInarySearch.cpp
@@ -1,5 +1,13 @@
 #include "BinarySearch.h"
 
 int main() {
+    BinarySearchTree b = BinarySearchTree();
+
+    b.insert(5);
+    b.insert(10);
+    b.insert(9);
+    b.insert(7);
+
+    b.search(7);
     return 0;
 }
--- BinarySearchTree/BinarySearch.h
+++ BinarySearchTree/BinarySearch.h
@@ -2,35 +2,79 @@
 #ifndef BINARY_SEARCH
 #define BINARY_SEARCH
 
-typedef struct TreeNode {
-    int data;
-    struct TreeNode *left, *right;
-} TreeNode;
+struct TreeNode {
+    int key;
+    // TreeNode를 가리키는 left, right 포인터 변수
+    TreeNode *left, *right;
+};
 
 class BinarySearchTree {
     private:
-        TreeNode* node;
-        int tree_size;
+        // TreeNode를 가리키는 포인터 변수
+        TreeNode *root;
 
     public:
         BinarySearchTree() {
-            tree_size = 0;
-            node = NULL;
+            root = NULL;
         }
 
-        TreeNode* search(int key) {
-            while (node != NULL) {
-                if (node->data == key) return node;
-                else if(node->data > key) {
-                    node = node->right;
+        void insert(int key) {
+            TreeNode *p, *t;
+
+            p = NULL;
+            t = root;
+
+            while (t != NULL) {
+                if (t->key == key) {
+                    std::cout << "입력하신 값이 이미 있어요." << std::endl;
+                    return;
                 }
-                else {
-                    node = node->left;
+
+                p = t;
+                if (key > t->key) {
+                    t = t->right;
+                } else {
+                    t = t->left;
+                }
+            }
+
+            TreeNode *n = new TreeNode;
+            if (n == NULL) return;
+
+            n->key = key;
+            n->left = n->right = NULL;
+
+            if (p != NULL) {
+                if (key > p->key) {
+                    p->right = n;
+                } else {
+                    p->left = n;
+                }
+            } else {
+                root = n;
+            }
+        }
+
+        TreeNode *search(int key) {
+            TreeNode *t = root;
+
+            while (t != NULL) {
+                if (t->key == key) {
+                    std::cout << "발견했어요!" << std::endl;
+                    return t;
+                }
+
+                else if(key > t->key) {
+                    t = t->right;
+                } else {
+                    t = t->left;
                 }
             }
 
             return NULL;
         }
+
+        
 };
 
 #endif
